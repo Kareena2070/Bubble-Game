fd6b509cc1464b9e3a054c7e52d49cad50c25c1e
Kareena2070 <kareenayadav1088@gmail.com>
2025-12-25 23:12:42 +0530

    add bubble game logic with random hits, countdown timer, and scoring

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function makeBubble(){
+let cutter = "";
+for(let i =0; i<=229; i++){
+    let num = Math.floor(Math.random()*10)
+    cutter += `<div class="bubble">${num}</div>`
+}
+document.querySelector(".panelB").innerHTML = cutter;
+}
+makeBubble();
+
+let time = 60;
+function timer(){
+    let timeInterval = setInterval(()=>{
+        if(time>0){
+            time--;
+            document.querySelector("#timer").textContent = time
+        }else{
+            clearInterval(timeInterval)
+            document.querySelector(".panelB").innerHTML=`<h1>Game Over</h1`;
+        }
+    }, 1000)
+
+}
+timer()
+
+
+let hitNumber = 0;
+function hitNum(){
+    hitNumber = Math.floor(Math.random()*10)
+    document.querySelector("#hitBubble").textContent = hitNumber
+}
+hitNum()
+
+let score = 0;
+function increseScore(){
+    score+=10;
+    document.querySelector("#scoreVal").textContent = score
+}
+
+
+document.querySelector(".panelB")
+.addEventListener("click",function(dets){
+    let clickNum = Number(dets.target.innerHTML)
+    console.log(clickNum)
+    if(clickNum === hitNumber){
+        increseScore()
+        makeBubble()
+        hitNum()
+    }
+})
